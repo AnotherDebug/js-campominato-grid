4bbf5eb4221b3837e3cc5ab6a41ad0c40a130c78
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-18 14:14:36 +0200

    [UPDATE]: file script

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+/* 
+**Consegna:**
+
+L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
+Ogni cella ha un numero progressivo, da 1 a 100.
+
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+==============================================
+
+1. Salvo il riferimento del mio container dove inserisco dinamicamente gli elementi 'div';
+
+2. Creo un ciclo che ad ogni iterazione genera il mio elemento 'div', che di fatto sarà un quadrato;
+
+3. Ogni quadrato avrà un evento al click, che aggiunge o toglie la classe active;
+*/
